8c87b1d91a12fa51c4aa62489e0c6d55553fef65
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-08-03 16:44:35 +0300

    Написали ввод массива

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите длину массива");
+int digit = Convert.ToInt32(Console.ReadLine());
+string[]array = new string[digit];
+
+
+void FillArray(string [] coll)
+{
+    int length = coll.Length;
+    for (int i = 0; i < length; i++)
+    {
+        System.Console.WriteLine("введите значение для массива");
+        string som = Console.ReadLine();
+        coll[i] = som;
+    }
+}
+FillArray(array);
+foreach (var item in array)
+{
+   Console.Write(" << " + item + ">>" + ", "); 
+}
